e851fa1de84ded7da1564b9b2182edf46c576e3b
OlofSjogren <olofsjogren97@gmail.com>
2021-12-10 16:38:41 +0100

    Day 10 complete

---
+++ src/day_10.js
@@ -0,0 +1,85 @@
+
+// ##################### Part 1 #####################
+
+const fs = require('fs')
+const content = fs.readFileSync(process.cwd() + "\\" + "day_10_data.txt").toString()
+const contentArr = content
+    .split("\r\n")
+    .map(row => row.split(""))
+
+const opening = ['(', '[', '{', '<']
+const closing = [')', ']', '}', '>']
+
+const checkTable = new Map([
+    [')', 3],
+    [']', 57],
+    ['}', 1197],
+    ['>', 25137]
+])
+
+
+const corruptedClosing = []
+const nonCorruptedLines = []
+
+contentArr.forEach(line => {
+    let corrupted = false
+    const stack = []
+    for (let i = 0; i < line.length; i++) {
+        const char = line[i]
+        if (opening.includes(char)) {
+            stack.push(char)
+        } else {
+            if (stack.length === 0) {
+                corruptedClosing.push(char)
+                break;
+            }
+            const top = stack.pop()
+            if (opening.indexOf(top) !== closing.indexOf(char)) {
+                corrupted = true
+                corruptedClosing.push(char)
+                break;
+            }
+        }
+    }
+    if (!corrupted) nonCorruptedLines.push(line)
+})
+
+const syntaxErrorScore = corruptedClosing.reduce((prev, char) => prev + checkTable.get(char), 0)
+
+// Answer part 1
+console.log({ syntaxErrorScore });
+
+// ##################### Part 2 #####################
+
+const autoCompleteTable = new Map([
+    ['(', 1],
+    ['[', 2],
+    ['{', 3],
+    ['<', 4]
+])
+
+const completionStrings = []
+nonCorruptedLines.forEach(line => {
+    const stack = []
+    for (let i = 0; i < line.length; i++) {
+        const char = line[i]
+        if (opening.includes(char)) {
+            stack.push(char)
+        } else {
+            stack.pop()
+        }
+    }
+    completionStrings.push(stack)
+})
+
+const middleScore = completionStrings
+    .map(line => line
+        .reverse()
+        .reduce((prev, char) => 
+            (prev*5) + autoCompleteTable.get(char), 0))
+    .sort((a,b) => a - b)[Math.floor(completionStrings.length / 2)]
+
+// Answer part 1
+console.log(middleScore);
+
+
