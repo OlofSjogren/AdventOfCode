eabd4034c0a4fa98769a5cf633d6329dfd080d9d
OlofSjogren <olofsjogren97@gmail.com>
2021-12-12 23:59:47 +0100

    Day 12 complete

---
+++ src/day_12.js
@@ -0,0 +1,63 @@
+
+// ##################### Part 1 (& 2) #####################
+
+const fs = require('fs')
+const content = fs.readFileSync(process.cwd() + "\\" + "day_12_data.txt").toString()
+const contentArr = content
+    .split("\r\n")
+    .map(row => row.split("-"))
+
+const isUpperCase = (string) => /^[A-Z]/.test(string)
+const findPaths = (node) => contentArr
+    .filter(path => path[0] === node || path[1] === node)
+    .map(path => path[0] === node ? path[1] : path[0])
+
+let paths = []
+const frontier = [["start"]]
+
+while (frontier.length !== 0) {
+    const onPath = frontier.pop()
+    const on = onPath.at(-1)
+    if (on === "end") {
+        paths.push(onPath)
+    } else {
+        //const visited = nodes.get(on)
+        //nodes.set(on, visited + 1)
+        const adjacent = findPaths(on)
+        adjacent.forEach(node => {
+            if (node !== "start" && (isUpperCase(node) || !onPath.includes(node))) {
+                frontier.push([...onPath, node])
+            }
+        })
+    }
+}
+
+console.log(`Answer Part 1 | Number of paths: ${paths.length}`);
+
+// ##################### Part 2 #####################
+// Not the most efficient way to do it... takes about 8 seconds to calculate on my machine
+
+const countSmallNodesVisitedTwice = path => {
+    let smallNodes = path.filter(node => !isUpperCase(node))
+    return smallNodes.length - (new Set(smallNodes)).size
+}
+
+let pathsPartTwo = []
+const frontierPartTwo = [[0, "start"]]
+
+while (frontierPartTwo.length !== 0) {
+    const onPath = frontierPartTwo.pop()
+    const on = onPath.at(-1)
+    if (on === "end") {
+        pathsPartTwo.push(onPath)
+    } else {
+        const adjacent = findPaths(on)
+        adjacent.forEach(node => {
+            if (node !== "start" && (isUpperCase(node) || countSmallNodesVisitedTwice(onPath) < 2)) {
+                frontierPartTwo.push([...onPath, node])
+            }
+        })
+    }
+}
+
+console.log(`Answer Part 2 | Number of paths: ${pathsPartTwo.length}`);
