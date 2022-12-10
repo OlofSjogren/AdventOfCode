713c31ebd52bc1c9af886ed8ab11254222b198b6
OlofSjogren <olofsjogren97@gmail.com>
2022-12-10 01:44:13 +0100

    Day 9 improvement

--- 2022/day_9/day_9.py
+++ 2022/day_9/day_9.py
@@ -1,23 +1,7 @@
 from common import read_data, display_result, print_matrix
 import numpy
-from collections import defaultdict
 
 max_dist_between = numpy.sqrt(2)
-
-
-def find_closest(p1, p2):
-    (x1, y1) = p1
-    (x2, y2) = p2
-    options = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
-    distances = [0.0] * len(options)
-    for i, (dx, dy) in enumerate(options):
-        x = x1 + dx
-        y = y1 + dy
-        dist_between = abs(numpy.linalg.norm((x2 - x, y2 - y)))
-        distances[i] = dist_between
-
-    min_dist_index = min(range(len(distances)), key=distances.__getitem__)
-    return options[min_dist_index]
 
 
 def take_steps(body, diff, steps, visited):
@@ -35,7 +19,8 @@ def take_steps(body, diff, steps, visited):
 
             dist_between = numpy.linalg.norm((front_x - part_x, front_y - part_y))
             if dist_between > max_dist_between:
-                [part_dx, part_dy] = find_closest((part_x, part_y), (front_x, front_y))
+                part_dx = numpy.sign(front_x - part_x)
+                part_dy = numpy.sign(front_y - part_y)
                 body[part_index][0] += part_dx
                 body[part_index][1] += part_dy
                 visited[f"{body[-1][0]},{body[-1][1]}"] = 1
@@ -44,7 +29,7 @@ def take_steps(body, diff, steps, visited):
 def run():
     data = read_data(1, test=False)
     moves = [line.split() for line in data.split("\n")]
-    directions = {  # [x,y]
+    directions = {  # (x,y)
         "U": (0, 1),
         "D": (0, -1),
         "R": (1, 0),
@@ -60,7 +45,6 @@ def run():
         take_steps(body_2, diff, int(steps), visited_2)
 
     visited_amount_2 = sum(visited_2.values())
-
     display_result(1, visited_amount_2)
 
     # ----------- PART 2 ----------- #
@@ -72,7 +56,6 @@ def run():
         take_steps(body_10, diff, int(steps), visited_10)
 
     visited_amount_10 = sum(visited_10.values())
-
     display_result(2, visited_amount_10)
 
 
